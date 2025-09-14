9e864a192cfd5fa5f426aaceb79bed9aa31f09bd
ashsouza <asouza@bearcubs.santarosa.edu>
2025-09-14 12:39:24 -0700

    Week4-commit

--- nextjs-blog/lib/posts.js
+++ nextjs-blog/lib/posts.js
@@ -65,16 +65,16 @@ export async function getPostData(id) {
   const fullPath = path.join(postsDirectory, `${id}.md`);
   const fileContents = fs.readFileSync(fullPath, 'utf8');
  
-  // Use gray-matter to parse the post metadata section
+  // gray-matter to parse/metadata
   const matterResult = matter(fileContents);
  
-  // Use remark to convert markdown into HTML string
+  // remark for HTML string
   const processedContent = await remark()
     .use(html)
     .process(matterResult.content);
   const contentHtml = processedContent.toString();
  
-  // Combine the data with the id and contentHtml
+  // combine data with id and contentHtml
   return {
     id,
     contentHtml,
